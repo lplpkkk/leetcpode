8bf174b104f5c2001886365cc4509824fd3a8092
Kenny yang <kuankuan0422@gmail.com>
2025-09-14 22:57:07 +0800

    Update 128__longest_consecutive_seq.rs

--- 128__longest_consecutive_seq.rs
+++ 128__longest_consecutive_seq.rs
@@ -1,20 +1,31 @@
 use std::collections::HashSet;
-impl Solution {
-    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        let set: HashSet<i32>=nums.iter().copied().collect();
-        let mut max_len=0;
 
-        for &num in &set{
-            if !set.contains(&(num-1)){
-                let mut cur=num;
-                let mut len=1;
-                while set.contains(&(cur+1)){
-                    len+=1;
-                    cur+=1;
-                }
-                max_len=max_len.max(len);
+/*
+/* only beats 47%*/
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut set:HashSet<i32>=HashSet::new();
+        for &i in &nums{
+            if set.contains(&i){
+                return true;
+            }else{
+                set.insert(i);
             }
         }
-        max_len
+        false
     }
 }
+*/
+/* this beats 88% */
+impl Solution {
+    pub fn contains_duplicate(mut nums: Vec<i32>) -> bool {
+        nums.sort_unstable();
+        for i in 1..nums.len(){
+            if nums[i]==nums[i-1]{
+                return true;
+            }
+        }
+        false
+    }
+}
+
