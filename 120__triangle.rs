dfb60dd3744224410f6af23027a5a86cc12003b8
Kenny yang <kuankuan0422@gmail.com>
2025-09-25 10:14:14 +0800

    Create 120__triangle.rs

---
+++ 120__triangle.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        let n=triangle.len();
+        if n==0{
+            return 0;
+        }
+
+        let mut dp=triangle[n-1].clone();
+
+        for i in (0..n-1).rev(){
+            for j in 0..triangle[i].len(){
+                dp[j]=triangle[i][j]+dp[j].min(dp[j+1]);
+            }
+        }
+        dp[0]
+    }
+}
