381f99a643a6e0926928816df93d4b8493360ff8
Kenny yang <kuankuan0422@gmail.com>
2025-11-18 09:03:18 +0800

    Create 717__1b_and_2b_char.rs
    easy居然要人幫

---
+++ 717__1b_and_2b_char.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
+        let n=bits.len();
+        let mut i=0;
+        while i<n-1{
+            if bits[i]==0{
+                i+=1;
+            }else{
+                i+=2;
+            }
+        }
+        i==n-1
+    }
+}
