9bd9c95ba20d4fdf997ca04c63e4e9e2b117ad18
Kenny yang <kuankuan0422@gmail.com>
2026-02-26 08:08:23 +0800

    Create 973__k_closest_points_to_origin.cpp
    sort的comparer

---
+++ 973__k_closest_points_to_origin.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(),points.end(),[](const vector<int>& a, const vector<int>& b){
+            return (a[0]*a[0]+a[1]*a[1]) < (b[0]*b[0]+b[1]*b[1]);
+        });
+        points.resize(k);
+        return points;
+    }
+};
