21b2e789d84f864f8913f59b2727eb0664c99a94
Kenny yang <kuankuan0422@gmail.com>
2026-02-28 10:34:54 +0800

    Create 191__num_of_1.cpp

---
+++ 191__num_of_1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans=0;
+        
+        while(n){
+            if(n&1){
+                ans+=1;
+            }
+            n>>=1;
+        }
+        return ans;
+    }
+};
