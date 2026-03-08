5a9331ccdb3fa7e47a189cea9019437e1eca752b
Kenny yang <kuankuan0422@gmail.com>
2026-03-08 10:41:42 +0800

    Create 1670__design_front_mid_back_q.cpp
    use deque

---
+++ 1670__design_front_mid_back_q.cpp
@@ -0,0 +1,82 @@
+class FrontMiddleBackQueue {
+    deque<int> left,right;
+    void balance(){
+        if (left.size()>right.size()){
+            right.push_front(left.back());
+            left.pop_back();
+        }
+        if(right.size()>left.size()+1){
+            left.push_back(right.front());
+            right.pop_front();
+        }
+    }
+
+public:
+    FrontMiddleBackQueue() {  
+    }
+    
+    void pushFront(int val) {
+        left.push_front(val);
+        balance();
+    }
+    
+    void pushMiddle(int val) {
+        if(left.size()==right.size()){
+            right.push_front(val);
+        }else{
+            left.push_back(val);
+        }
+    }
+    
+    void pushBack(int val) {
+        right.push_back(val);
+        balance();
+    }
+    
+    int popFront() {
+        if(right.empty()) return -1;
+        int val;
+        if(left.empty()){
+            val=right.front();
+            right.pop_front();
+        }else{
+            val=left.front();
+            left.pop_front();
+        }
+        balance();
+        return val;
+    }
+    
+    int popMiddle() {
+        if(right.empty()) return -1;
+        int val;
+        if(left.size()==right.size()){
+            val=left.back();
+            left.pop_back();
+        }else{
+            val=right.front();
+            right.pop_front();
+        }
+        balance();
+        return val;
+    }
+    
+    int popBack() {
+        if(right.empty()) return -1;
+        int val=right.back();
+        right.pop_back();
+        balance();
+        return val;
+    }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
