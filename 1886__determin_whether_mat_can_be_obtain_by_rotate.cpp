d678470e74486a517396bcaaaca9fc18ee8d1fa8
Kenny yang <kuankuan0422@gmail.com>
2026-03-22 10:22:57 +0800

    Create 1886__determin_whether_mat_can_be_obtain_by_rotate.cpp
    你要知道怎麼轉90度:
    reverser+swap對角線

---
+++ 1886__determin_whether_mat_can_be_obtain_by_rotate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for(int i=0;i<4;i++){
+            if(mat==target){
+                return true;
+            }
+            rotate(mat);
+        }        
+        return false;
+    }
+
+private:
+    void rotate(vector<vector<int>>& mat){
+        int n=mat.size();
+        reverse(mat.begin(),mat.end());
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+    }
+};
