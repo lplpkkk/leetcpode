af90feda765989ed9b826ed28afb1a6e138b55fb
Kenny yang <kuankuan0422@gmail.com>
2026-08-09 20:26:38 +0800

    Create 92__revll2_260809.c
    插頭法

---
+++ 92__revll2_260809.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
+
+    struct ListNode dummy={0,head};
+    struct ListNode* prev=&dummy;
+
+    for(int i=1;i<left;i++){
+        prev=prev->next;
+    }
+
+    struct ListNode* cur=prev->next;
+
+    for(int i=0;i<right-left;i++){
+        struct ListNode* next=cur->next;
+
+        cur->next=next->next;
+        next->next=prev->next;
+        prev->next=next;
+    }
+
+    return dummy.next;
+}
