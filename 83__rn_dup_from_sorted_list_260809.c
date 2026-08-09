97244431254b6f405e14ccd5ecf83d9f7d6bf6ea
Kenny yang <kuankuan0422@gmail.com>
2026-08-09 22:56:35 +0800

    Create 83__rn_dup_from_sorted_list_260809.c

---
+++ 83__rn_dup_from_sorted_list_260809.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* cur=head;
+
+    while(cur&&cur->next){
+        if(cur->val==cur->next->val){
+            cur->next=cur->next->next;
+        }else{
+            cur=cur->next;
+        }
+    }
+
+    return head;
+}
