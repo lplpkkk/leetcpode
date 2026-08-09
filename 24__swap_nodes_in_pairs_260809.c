24d08035eef77fbb5bcf93748f5807e3fee5e173
Kenny yang <kuankuan0422@gmail.com>
2026-08-09 13:52:30 +0800

    Create 24__swap_nodes_in_pairs_260809.c
    要紀清楚順序
    prev直接先接上second, first接到second next之後,再把scond接到first, 然後就能準備下一輪, prev設到first, head=first->next

---
+++ 24__swap_nodes_in_pairs_260809.c
@@ -0,0 +1,20 @@
+
+ 
+struct ListNode* swapPairs(struct ListNode* head) {
+    struct ListNode dummy={0,head};
+    struct ListNode* prev=&dummy;
+
+    while(head&&head->next){
+        struct ListNode* first=head;
+        struct ListNode* second=head->next;
+
+        prev->next=second;
+        first->next=second->next;
+        second->next=first;
+
+        prev=first;
+        head=first->next;
+    }
+
+    return dummy.next;
+}
